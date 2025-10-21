84759c4db1a0e6cbcb83571c571935fb9c555c83
Ali Murtaza <aliarain1404@gmail.com>
2025-10-21 18:31:06 +0500

    first commit

---
+++ app/shop/collection/[collection]/page.jsx
@@ -0,0 +1,18 @@
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+import CollectionPage from "@/components/BridalandEvening";
+
+
+
+export default function Dress() {
+  return (
+    <>
+      <Header />
+     
+      <CollectionPage />
+     <Footer />
+           
+          </>
+        );
+      }
